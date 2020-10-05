6de4b7e0015383b5fad29bae1386147ee75144d1
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-04 20:06:32 -0700

    small changes

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-};
+  app.use(express.static("client/build"));
+}
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
